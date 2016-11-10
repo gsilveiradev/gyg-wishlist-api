09859459c4d44c6c3888ff5f22f08acccf631830
Guilherme Silveira da Silva <guissilveira@gmail.com>
2016-11-10 14:21:47 -0200

    Start project with Laravel Framework Copy. Create structure for routes with api versioning. Create endpoint for Authentication.

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# gyg-wishlist-api
-Wishlist API built with Laravel5
+# Wishlist API
+
+This project was built with Laravel Framework 5.3.
+
+## Install the Project
+
+Clone repository and then do the composer install:
+
+```bash
+composer install
+```
+
+Configure your ```.env``` file. Do not forget to create a MySQL database and configure it in .env file.
+
+### Migrations
+
+All the entities and tables are represented by Models and are created in db with Laravel migrations.
+
+Models are in:
+
+```
+app/
+   > User.php
+```
+
+Migrations are in:
+
+```
+database/migrations/*
+```
+
+Run migrations and Seed (to create a dummy data)
+
+```bash
+php artisan migrate:refresh --seed
+```
+
+### Routes
+
+All the routes os this api are in ```routes/api.php```.
+
+Real GET examples:
+
+```
+http://localhost:8000/api/exercises/
+http://localhost:8000/api/plans/
+http://localhost:8000/api/users/
+```
+
+### Test
+
+Run the serve command:
+
+```bash
+php artisan serve
+```
+
+## Tickets priorization
+
+1. Create API for adding, removing and listing activities in wishlist
+2. Add authentication through tokens for security
+3. Add API versioning support so we can expand without regressions
+4. Add possibility to support multiple wishlists per user
+5. Allow customers to also wishlist Locations (Points of Interest, Cities, etc) (v2?)
+
+### Understanding
+
+First release: items 1, 2, 3 and 4.
+
+I wanted to choose the ticket numbers 1, 2, 3 because they are the base of the API.
+Number 1 is the goal of this project.
+Number 2 represents the authentication used to do relation between users and wishlists feature, also it is responsible for the security between api endpoits.
+Number 3 represents another base requirement in order to expand the api by versioning.
+Number 4 is include because it is not a hard feature if you think before coding.
+
+Second release: item 4.
---
+++ routes/api.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['namespace' => 'Api'], function () {
+    /*
+    |--------------------------------------------------------------------------
+    | Exclusive routes for Version 1
+    |--------------------------------------------------------------------------
+    */
+    Route::group(['namespace' => 'V1', 'prefix' => 'v1'], function () {
+        /*
+        |--------------------------------------------------------------------------
+        | Exclusive routes for authentication service
+        |--------------------------------------------------------------------------
+        */
+        Route::group(['namespace' => 'Authentication'], function () {
+            Route::post('/authentication', 'AuthenticationController@authenticate');
+            Route::post('/authentication/forgot_password', 'AuthenticationController@forgotPassword');
+
+            /*
+            |--------------------------------------------------------------------------
+            | Routes with required login
+            |--------------------------------------------------------------------------
+            */
+            Route::group(['middleware' => ['jwt.auth']], function () {
+
+                Route::put('/authentication/change_password', 'AuthenticationController@changePassword');
+                Route::post('/authentication/logout', 'AuthenticationController@logout');
+
+                Route::get('/authentication/refresh_token', 'AuthenticationController@refreshToken');
+            });
+        });
+    });
+        
+});
